251d7e4096ed3585db4ee7a18328cfd0ef237f71
danielfersanchez <38057662+danielfersanchez@users.noreply.github.com>
2018-04-21 14:25:46 -0500

    comiet

---
+++ controlesdinamicos/controlesdinamicos/controlesdinamicos/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace controlesdinamicos
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            boton.Clicked += Boton_Clicked;// creacion del evento del boton
+		}
+
+        private void Boton_Clicked(object sender, EventArgs e)
+        {
+            panel.Children.Add(new Button() {
+                Text = $"boton{panel.Children.Count+1}"//formateada
+            });
+        }
+    }
+}
